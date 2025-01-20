d47120a673e1252892a7d49546d364467d83a53a
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-01-20 11:46:51 +0000

    Using `eval` for Homebrew shellenv
    I don't like it, though.`eval` should be avoided outright.

--- profile.d/app/brew.zsh
+++ profile.d/app/brew.zsh
@@ -5,7 +5,7 @@
 ### Loading shell environment
 if [[ -d /home/linuxbrew/.linuxbrew && -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
 	if ! haspath /home/linuxbrew/.linuxbrew/bin || ! haspath /home/linuxbrew/.linuxbrew/sbin; then
-		. "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 		# export PKG_CONFIG_PATH="$(pkg-config --variable pc_path pkg-config):$(/usr/bin/pkg-config --variable pc_path pkg-config)"
 	fi
