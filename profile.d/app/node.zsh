fbf01ad0d4db098e75be13acbea902691b647228
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-02-25 19:01:58 +0000

    Adding Node flatpak as main binary was a terrible idea
    I knew this going in, and yet I stubbornly tried it anyway.

--- profile.d/app/node.zsh
+++ profile.d/app/node.zsh
@@ -3,10 +3,3 @@
 #######################################
 
 # Find npm package to add to PATH
-## Check for Flatpak version
-! printenv FLATPAK_ENV &>/dev/null && zsource flatpak
-if flatpak-has -i node; then
-	NODE_HOME=$(echo ${FLATPAK_ENV[USER_DIR]}/runtime/org.freedesktop.Sdk.Extension.node<->/x86_64/24.08/active/files/bin(OcFY1))
-	[[ -d "$NODE_HOME" ]] && addpath "$NODE_HOME"
-	unset NODE_HOME
-fi
