291ab3019c90cf0d2294ed2fe4c68e9578b4ad81
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-03-05 01:35:36 +0000

    Preventing calls to to-be-sourced functions in plugins

--- custom/plugins/npm/npm.plugin.zsh
+++ custom/plugins/npm/npm.plugin.zsh
@@ -1,9 +1,7 @@
 (( ${+commands[npm]} )) || return 0
 
 # If NPM_CONFIG_USERCONFIG is set, check if file exists and if it contains a given configuration
-if (( ${+NPM_CONFIG_USERCONFIG} )) && \grep -Eq 'prefix = ${XDG_DATA_HOME}/npm' "$NPM_CONFIG_USERCONFIG" && \
-	! file_contents_in "$ZDOTDIR/conf/npm/.npmrc" "$NPM_CONFIG_USERCONFIG"
-then
+if (( ${+NPM_CONFIG_USERCONFIG} )) && \grep -Eq 'prefix = ${XDG_DATA_HOME}/npm' "$NPM_CONFIG_USERCONFIG"; then
 	diff -BNPZbrw --changed-group-format='%>' --unchanged-group-format='' --to-file "$ZDOTDIR/conf/npm/.npmrc" "$NPM_CONFIG_USERCONFIG" > "$XDG_CACHE_HOME"/zsh/npmrc.diff
 	cat "$XDG_CACHE_HOME"/zsh/npmrc.diff >> "$NPM_CONFIG_USERCONFIG"
 	rm -f "$XDG_CACHE_HOME"/zsh/npmrc.diff
@@ -15,7 +13,9 @@ _npm_pfx="$(npm config get prefix)"
 (( ${(v)+path[(I)"$_npm_pfx"/bin]} )) || path=("$_npm_pfx/bin" $path)
 
 # Append global node_modules to MANPATH
-(( ${+MANPATH} )) && addvar MANPATH "$_npm_pfx/share"
+if (( ${+MANPATH} )) && [[ "$MANPATH" =~ ":?$_npm_pfx/share:?" ]]; then
+	MANPATH+=":$_npm_pfx/share"
+fi
 
 unset _npm_pfx
 
