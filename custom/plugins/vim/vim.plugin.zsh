dc81db1a0c6f1c5cd6025af3ae898f51b3f1378d
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-03-16 12:32:48 +0000

    Added vim plugin, with base vim configuration

---
+++ custom/plugins/vim/vim.plugin.zsh
@@ -0,0 +1,10 @@
+(( ${+commands[vim]} )) || return
+
+# Create vim config path and set user vimrc
+[[ -d "${XDG_CONFIG_HOME}/vim" ]] || mkdir -p "${XDG_CONFIG_HOME}/vim"
+export MYVIMRC="${XDG_CONFIG_HOME}/vim/vimrc"
+
+# Copy the whole vimrc file. Any user plugin should be loaded in a 'plugins' file, or additional configuration in 'myvimrc'
+if diff -q "$ZDOTDIR/conf/vim/vimrc" "$MYVIMRC" &>/dev/null; then
+	cp "$ZDOTDIR/conf/vim/vimrc" "$MYVIMRC"
+fi
