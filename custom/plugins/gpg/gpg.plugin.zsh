a2ad54ade02d8aac9611829da782323e654da1aa
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-04-08 18:33:06 +0100

    `gpg`: Minor plugin fix

--- custom/plugins/gpg/gpg.plugin.zsh
+++ custom/plugins/gpg/gpg.plugin.zsh
@@ -8,6 +8,6 @@ function gpg-fix-perms {
 		print_fn -e "Unable to fix permissions due to invalid path: '$homedir'"
 		return 1
 	fi
-	chmod 600 "$GNUPGHOME"/{**,.*/**}/*(-.) # Set 600 for files
-	chmod 700 "$GNUPGHOME"/{**,.*/**}/(-/)  # Set 700 for directories
+	chmod 600 "$homedir"/{**,.*/**}/*(-.) # Set 600 for files
+	chmod 700 "$homedir"/{**,.*/**}/(-/)  # Set 700 for directories
 }
