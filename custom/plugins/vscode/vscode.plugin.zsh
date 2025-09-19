4f1b15a0751f5fbe22cf66bcbb1269553c484b09
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-09-19 16:57:50 +0100

    `vscode`: Fixes and improvements
    Changes:
    - Added support for VSCodium Flatpak.
    - Removed a few envvars and session buses that were fixed in the base VSCode Flatpak version.
    - Added Sonarlint override only if its extension is installed.
    - Added messages and return values upon setting Flatpak overrides.

--- custom/plugins/vscode/vscode.plugin.zsh
+++ custom/plugins/vscode/vscode.plugin.zsh
@@ -1,23 +1,27 @@
 ### VScode (Flatpak)
-if (( ${+commands[com.visualstudio.code]} )); then
-	# Point Flatpak's .ssh config to user's config
-	(_vscode_dir="$HOME"/.var/app/com.visualstudio.code
-		_ssh_dir="$_vscode_dir"/.ssh
-		_ssh_configfile="$_ssh_dir"/config
-		# Prepare ssh_config
-		if [[ ! -f "$_ssh_configfile" ]] || ! \grep -q "Include ${SSH_HOME:-$HOME/.ssh}/config.d" "$_ssh_configfile"; then
-			[[ -d "$_ssh_dir" ]] || mkdir -p "$_ssh_dir"
-			echo "Include ${SSH_HOME:-$HOME/.ssh}/config.d/*" >> "$_ssh_configfile"
-		fi
-		# Prepare ssh link
-		if [[ ! -L "$_vscode_dir/config/ssh" ]]; then
-			ln -s "${SSH_HOME:-$HOME/.ssh}" "$_vscode_dir/config/ssh"
-		fi
-	)
+if (( ${(v)#commands[(I)com.visualstudio.code|com.vscodium.codium]} )); then
+	for _vscode in ${(k)commands[(I)com.visualstudio.code|com.vscodium.codium]}; do
+		# Point Flatpak's .ssh config to user's config
+		(_vscode_dir="$HOME"/.var/app/$_vscode
+			_ssh_dir="$_vscode_dir"/.ssh
+			_ssh_configfile="$_ssh_dir"/config
+			# Prepare ssh_config
+			if [[ ! -f "$_ssh_configfile" ]] || ! \grep -q "Include ${SSH_HOME:-$HOME/.ssh}/config.d" "$_ssh_configfile"; then
+				[[ -d "$_ssh_dir" ]] || mkdir -p "$_ssh_dir"
+				echo "Include ${SSH_HOME:-$HOME/.ssh}/config.d/*" >> "$_ssh_configfile"
+			fi
+			# Prepare ssh link
+			if [[ ! -L "$_vscode_dir/config/ssh" ]]; then
+				ln -s "${SSH_HOME:-$HOME/.ssh}" "$_vscode_dir/config/ssh"
+			fi
+		)
+	done
+	unset _vscode
 
 	# Sets VScode Flatpak's overrides
 	function vscode-flatpak-overrides {
-		local -r flatpak_app="com.visualstudio.code"
+		local -i retval=0
+		local -r flatpak_app="${commands[(i)com.visualstudio.code|com.vscodium.codium]}"
 
 		# Setup symlinks
 		local -r VSCODE_HOME="${FLATPAK_ENV[USER_APPDATA]:-$HOME/.var/app}/$flatpak_app"
@@ -31,6 +35,7 @@ if (( ${+commands[com.visualstudio.code]} )); then
 			if [[ -e "$vs_src" ]] && [[ ! -L "${VSCODE_HOME}/$vs_target" ]]; then
 				[[ -e "${VSCODE_HOME}/$vs_target" ]] && rm -rf "${VSCODE_HOME}/$vs_target"
 				ln -s "$vs_src" "${VSCODE_HOME}/$vs_target"
+				retval+=$?
 			fi
 		done
 
@@ -41,34 +46,43 @@ if (( ${+commands[com.visualstudio.code]} )); then
 		local -A VSCODE_ENVVARS=(
 			[HOST_HOME]="$HOME"
 			[HOME]="$VSCODE_HOME"
-			[ELECTRON_OZONE_PLATFORM_HINT]=auto
-			[ELECTRON_TRASH]=trash
-			[TRASHDIR]="${XDG_DATA_HOME}/Trash"
-			[SONARLINT_USER_HOME]="${XDG_DATA_HOME}/sonarlint"
 		)
+
+		# Add .NET path
 		(( ${+DOTNET_ROOT} )) && VSCODE_ENVVARS[DOTNET_ROOT]="$DOTNET_ROOT"
+
+		# Add Sonarlint path
+		case $(find ~/.var/app/"$flatpak_app"/data/*/extensions -maxdepth 1 -type d -name "*sonarlint*" -print -quit) in
+		'') (( ${+SONARLINT_USER_HOME} )) && VSCODE_ENVVARS[SONARLINT_USER_HOME]="${XDG_DATA_HOME}/sonarlint" ;;
+		esac
 
 		local vs_envkey vs_envval
 		for vs_envkey vs_envval in ${(@kv)VSCODE_ENVVARS}; do
 			if ! \grep -q "^$vs_envkey=$vs_envval\$" "${VSCODE_OVERRIDES_FILE}"; then
-				flatpak --user override $flatpak_app --env="$vs_envkey=$vs_envval"
+				flatpak --user override "$flatpak_app" --env="$vs_envkey=$vs_envval"
+				retval+=$?
 			fi
 		done
 
 		# Setup session bus
 		local -a VSCODE_DBUS_TALK=(
-			org.kde.kwalletd
 		)
 
 		local vs_dbus_talk
 		for vs_dbus_talk in ${VSCODE_DBUS_TALK}; do
 			if ! \grep -q "^$vs_dbus_talk=talk\$" "${VSCODE_OVERRIDES_FILE}"; then
 				flatpak --user override $flatpak_app --talk-name="$vs_dbus_talk"
+				retval+=$?
 			fi
 		done
+
+		if (( ! $retval )); then
+			print_fn -i "All overrides for $flatpak_app were set."
+		else
+			print_fn -w "Errors occured while setting overrides for $flatpak_app."
+		fi
+		return $retval
 	}
 elif (( ${+commands[code]} )); then
 	: # Add config for regular VScode installation
-else
-	return 1
 fi
