a67eaa31fabcd617ecf0847c49cfa2654c812a47
António Sarmento (Keyaku) <4369174+Keyaku@users.noreply.github.com>
2025-10-25 18:14:35 +0100

    `android`: Fixed development envvars
    Changes:
    - Corrected `ANDROID_*` envvars.
    - Fixed `rish: File exists` error.
    - Added simple `java` extension.

--- extensions/android/android.plugin.zsh
+++ extensions/android/android.plugin.zsh
@@ -5,14 +5,34 @@ if ! whatami Android; then
 		alias shizuku-start='adb shell sh /sdcard/Android/data/moe.shizuku.privileged.api/start.sh'
 
 		# Android debugging
-		export ANDROID_HOME="${XDG_DATA_HOME}/android"
-		[[ -d "$ANDROID_HOME" ]] || mkdir -p "$ANDROID_HOME"
-		export ANDROID_USER_HOME="${ANDROID_HOME}/.android"
+		export ANDROID_ROOT="${XDG_DATA_HOME}/android"
+		[[ -d "$ANDROID_ROOT" ]] || mkdir -p "$ANDROID_ROOT"
+		export ANDROID_USER_HOME="${ANDROID_ROOT}/.android"
 
 		if (( ${+commands[adb]} )); then
 			# Prevent adb from using user's home directory
-			alias adb="HOME=$ANDROID_HOME ${commands[adb]}"
-			alias fastboot="HOME=$ANDROID_HOME ${commands[fastboot]}"
+			alias adb="HOME=$ANDROID_ROOT ${commands[adb]}"
+			alias fastboot="HOME=$ANDROID_ROOT ${commands[fastboot]}"
+		fi
+
+		# Android development. Prefer sdk/ over ndk/
+		if [[ -d "${ANDROID_ROOT}"/sdk ]]; then
+			# Contrary to search results, do NOT set this path as ANDROID_SDK_ROOT. Set it as ANDROID_HOME:
+			export ANDROID_HOME="${ANDROID_ROOT}"/sdk
+			# Pick the latest NDK version found (tests non-empty directories)
+			ANDROID_NDK_ROOT="$(echo "$ANDROID_ROOT/sdk/ndk"/*(OnFN/[1]))"
+			if [[ "$ANDROID_NDK_ROOT" ]]; then
+				export ANDROID_NDK_ROOT
+				addpath "$ANDROID_NDK_ROOT"
+			else
+				unset ANDROID_NDK_ROOT
+			fi
+
+			export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+			#export _JAVA_OPTIONS+=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+
+			# Prepend platform tools to override system installation
+			addpath -p "${ANDROID_ROOT}/sdk/platform-tools"
 		fi
 	fi
 # if actually on Android (Termux)
@@ -26,7 +46,10 @@ elif (( ${+TERMUX_VERSION} )); then
 		(rish_exec=($(echo "$HOME"/.termux/rish.d/rish(.NxE)))
 			(( 0 == ${#rish_exec} )) && chmod ug+x "$HOME"/.termux/rish.d/rish
 		)
-		[[ -L "$HOME"/.local/bin/rish ]] || ln -s "$HOME"/.termux/rish.d/rish "$HOME"/.local/bin/rish
+		if [[ ! -L "$HOME"/.local/bin/rish ]]; then
+			rm -f "$HOME"/.local/bin/rish
+			ln -s "$HOME"/.termux/rish.d/rish "$HOME"/.local/bin/rish
+		fi
 		[[ "${RISH_APPLICATION_ID}" == "com.termux" ]] || export RISH_APPLICATION_ID="com.termux"
 	fi
 else
