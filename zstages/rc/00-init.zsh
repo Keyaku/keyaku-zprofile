7bcd0e63ce43c1ec227026745cea5aa3d4ee9649
António Sarmento <Keyaku@users.noreply.github.com>
2026-03-11 13:01:16 +0000

    Initializing submodules via `zupdate`

--- zstages/rc/00-init.zsh
+++ zstages/rc/00-init.zsh
@@ -1,7 +1,6 @@
 # Initialize submodules if ohmyzsh is not present or empty
 [[ "${ZPROFILE_MODULES}" ]] || ZPROFILE_MODULES=($(git -C "$ZDOTDIR" config --file .gitmodules --get-regexp path | awk '{ print $2 }'))
 if [[ "$(echo "$ZDOTDIR"/$^ZPROFILE_MODULES/(N^F))" ]]; then
-	echo "Initializing submodules..."
 	# Initialize submodules
-	git -C "$ZDOTDIR" submodule -q update --init --remote --recursive
+	zupdate --submodules
 fi
