d0acf125da13a4064f545d44d4e01a2718f628d3
António Sarmento <Keyaku@users.noreply.github.com>
2026-03-17 14:11:45 +0000

    Improved bash completion code

--- zstages/rc/30-completion.zsh
+++ zstages/rc/30-completion.zsh
@@ -1,7 +1,9 @@
 # Bash modules & autocompletion (for programs which contain only bash completions)
 if [[ -d "$XDG_DATA_HOME"/bash-completion/completions ]]; then
+	# Presume bashcompinit was already executed if it's loaded
+	(( ${+functions[bashcompinit]} )) || { autoload -U +X bashcompinit && bashcompinit; }
+
 	local f_bashcomp
-	autoload bashcompinit && bashcompinit &&
 	for f_bashcomp in "$XDG_DATA_HOME"/bash-completion/completions/*(-N.); do
 		source "$f_bashcomp"
 	done
