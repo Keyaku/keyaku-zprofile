abc30577faca9538c0fe0998d95720d076ca85b1
António Sarmento <Keyaku@users.noreply.github.com>
2026-03-19 19:15:11 +0000

    Optimized plugin loader further
    Changes:
    - Reduced fpath plugin search from a loop to zsh expansions.
    - Used new array of results for the plugin loader.

--- zstages/rc/25-omz-load.zsh
+++ zstages/rc/25-omz-load.zsh
@@ -8,26 +8,14 @@
 # ============================================================================
 # Prepare fpath for plugins before omz loads
 # ============================================================================
-local -A plugin_map=()
-
 local -a plugin_dirs=("$ZSH_CUSTOM" "$ZSH")
 local -a plugin_results
-local plugin plugin_found
 
-for plugin ($plugins); do
-	plugin_results=(${^plugin_dirs}/plugins/$plugin/{_*,$plugin.plugin.zsh}(.N))
-	if (( ! ${#plugin_results} )); then
-		print_fn -w "plugin '$plugin' not found"
-		continue
-	fi
-	for plugin_found ($plugin_results); do
-		if [[ "${plugin_found:e}" == "zsh" ]]; then
-			plugin_map[$plugin]="${plugin_found:h}"
-		elif [[ "${plugin_found:t}" == "_$plugin" ]]; then
-			fpath=("${plugin_found:h}" $fpath)
-		fi
-	done
-done
+# Gather all directories with a .plugin.zsh
+plugin_results=(${^plugin_dirs}/plugins/${^plugins}/*.plugin.zsh(.N))
+
+# Update fpath with directories containing a completion (_*) file
+fpath=(${^plugin_dirs}/plugins/${^plugins}/_*(.N[1]:h) $fpath)
 
 # ============================================================================
 # Load ohmyzsh
@@ -44,25 +32,24 @@ _zsh_source_file "$ZSH"/oh-my-zsh.sh
 plugins=($_plugins)
 
 local -A aliases_pre galiases_pre
-local plugin_file plugin_dir disable_aliases
-for plugin plugin_dir in ${(kv)plugin_map}; do
-	plugin_file="${plugin_dir}/$plugin.plugin.zsh"
+local plugin plugin_file disable_aliases
+
+for plugin_file in ${plugin_results}; do
+	plugin="${plugin_file:h:t}"
 
 	disable_aliases=0
 	zstyle -T ":omz:plugins:$plugin" aliases || disable_aliases=1
 
 	if (( disable_aliases )); then
 		aliases_pre=("${(@kv)aliases}")
-		galiases_pre=("${(@kv)galiases}")
 
 		_zsh_source_file "$plugin_file"
 
 		aliases=("${(@kv)aliases_pre}")
-		galiases=("${(@kv)galiases_pre}")
 	else
 		_zsh_source_file "$plugin_file"
 	fi
 done
 
-# Clear fpath duplicates added by omz. Stays commented just in case.
-# fpath=(${(u)fpath})
+# Clear fpath duplicates added by omz. Comment this line in case fpath is bloated
+fpath=(${(u)fpath})
