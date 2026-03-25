b67dd499a763ffa29b6e98fc8125d8753344ec5d
António Sarmento <Keyaku@users.noreply.github.com>
2026-03-25 09:52:55 +0000

    `25-omz-load.zsh`: Added benchmark measurements

--- zstages/rc/25-omz-load.zsh
+++ zstages/rc/25-omz-load.zsh
@@ -5,6 +5,12 @@
 # Check for Oh-My-ZSH; stop processing if not found
 [[ -n "$ZSH" && -f "${ZSH}/oh-my-zsh.sh" ]] || return
 
+if (( ${ZSH_PROFILE_BENCHMARK} )); then
+	zmodload zsh/datetime
+	local -F _tfpath0 _tfpath1 _t0 _t1 _t2 _t3 _tsources _tploader0 _tploader1
+	_tfpath0=$EPOCHREALTIME
+fi
+
 # ============================================================================
 # Prepare fpath for plugins before omz loads
 # ============================================================================
@@ -30,6 +36,10 @@ done
 # Update fpath
 fpath=(${fpaths_found} $fpath)
 
+if (( ${ZSH_PROFILE_BENCHMARK} )); then
+	print "fpath prep: $(( (EPOCHREALTIME-_tfpath0) ))s"
+	# printf '>>> %s: %s\n' "fpaths_found" "${fpaths_found}" plugins_found "${plugins_found}"
+fi
 
 # ============================================================================
 # Load ohmyzsh
@@ -48,22 +58,40 @@ plugins=($_plugins)
 local -A aliases_pre
 local plugin_file disable_aliases
 
+if (( ${ZSH_PROFILE_BENCHMARK} )); then
+	_tploader0=$EPOCHREALTIME
+fi
+
 for plugin_file in ${plugins_found}; do
 	plugin="${plugin_file:h:t}"
 
+	# (( ${ZSH_PROFILE_BENCHMARK} )) && _t0=$EPOCHREALTIME
 	disable_aliases=0
 	zstyle -T ":omz:plugins:$plugin" aliases || disable_aliases=1
+	# (( ${ZSH_PROFILE_BENCHMARK} )) && _t1=$EPOCHREALTIME
 
 	if (( disable_aliases )); then
 		aliases_pre=("${(@kv)aliases}")
 
+		(( ${ZSH_PROFILE_BENCHMARK} )) && _t2=$EPOCHREALTIME
 		_zsh_source_file "$plugin_file"
+		(( ${ZSH_PROFILE_BENCHMARK} )) && _t3=$EPOCHREALTIME
 
 		aliases=("${(@kv)aliases_pre}")
 	else
+		(( ${ZSH_PROFILE_BENCHMARK} )) && _t2=$EPOCHREALTIME
 		_zsh_source_file "$plugin_file"
+		(( ${ZSH_PROFILE_BENCHMARK} )) && _t3=$EPOCHREALTIME
 	fi
+	(( ${ZSH_PROFILE_BENCHMARK} )) && _tsources=$((_tsources + (_t3-_t2)))
 done
+
+if (( ${ZSH_PROFILE_BENCHMARK} )); then
+	_tploader1=$EPOCHREALTIME
+	print "plugin loader: $((_tploader1-_tploader0 - _tsources))s"
+	# print "plugin sources: $((_tsources))s"
+	# print "plugin total: $((_tploader1-_tploader0))s"
+fi
 
 # Clear fpath duplicates added by omz. Comment this line in case fpath is bloated
 fpath=(${(u)fpath})
