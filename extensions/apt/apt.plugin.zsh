5c71432e67bd14b6a9a686fb11f0a5fa2da59c0d
António Sarmento <Keyaku@users.noreply.github.com>
2026-03-31 10:06:11 +0100

    Attempted fix at `apt` aliases not being registered in history

--- extensions/apt/apt.plugin.zsh
+++ extensions/apt/apt.plugin.zsh
@@ -3,16 +3,16 @@ whatami Debian Ubuntu Android || return
 
 (( ${+commands[apt]} || ${+commands[apt-get]} )) || return
 
-SUDO=$(whatami Android || echo sudo)
+local SUDO=$(whatami Android || echo "sudo ")
 
 ### APT-related aliases
-alias apt-update="$SUDO apt update"
-alias apt-upgrade="$SUDO apt upgrade -y --auto-remove"
-alias apt-install="$SUDO apt install -y --auto-remove"
-alias apt-remove="$SUDO apt remove -y --auto-remove"
+alias apt-update="${SUDO}apt update"
+alias apt-upgrade="${SUDO}apt upgrade -y --auto-remove"
+alias apt-install="${SUDO}apt install -y --auto-remove"
+alias apt-remove="${SUDO}apt remove -y --auto-remove"
 alias apt-purge="apt-remove --purge"
-alias apt-autoremove="$SUDO apt autoremove --purge -y"
+alias apt-autoremove="${SUDO}apt autoremove --purge -y"
 alias apt-all="apt-update && apt-upgrade"
-alias apt-clean="$SUDO apt-get clean"
-alias apt-markauto="$SUDO apt-mark auto"
+alias apt-clean="${SUDO}apt-get clean"
+alias apt-markauto="${SUDO}apt-mark auto"
 alias apt-search="apt-cache search"
