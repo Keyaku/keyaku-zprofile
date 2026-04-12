58b29e6d08449321e95df5362515c7a61ba938ac
António Sarmento <Keyaku@users.noreply.github.com>
2026-04-12 17:05:23 +0100

    `caddy`: Adding "overwrite" flag

--- extensions/caddy/caddy.plugin.zsh
+++ extensions/caddy/caddy.plugin.zsh
@@ -3,4 +3,4 @@
 # FIXME: This works better with the default caddy config (which also works with docker)
 # but not with a user-defined path.
 alias caddy-reload="caddy reload --config /etc/caddy/Caddyfile"
-alias caddy-fmt="caddy fmt /etc/caddy/Caddyfile"
+alias caddy-fmt="caddy fmt --overwrite /etc/caddy/Caddyfile"
