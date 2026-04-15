0fb232a7875d73a7a710aa4a64c385bb5b4926d2
António Sarmento <Keyaku@users.noreply.github.com>
2026-04-15 11:37:06 +0100

    `apt`: Removed `-y` flag to prevent dangerous situations
    Just kept it for upgrading since it seems the only situation where advancing unprompted is intended.

--- extensions/apt/apt.plugin.zsh
+++ extensions/apt/apt.plugin.zsh
@@ -8,10 +8,10 @@ local SUDO=$(whatami Android || echo "sudo ")
 ### APT-related aliases
 alias apt-update="${SUDO}apt update"
 alias apt-upgrade="${SUDO}apt upgrade -y --auto-remove"
-alias apt-install="${SUDO}apt install -y --auto-remove"
-alias apt-remove="${SUDO}apt remove -y --auto-remove"
+alias apt-install="${SUDO}apt install --auto-remove"
+alias apt-remove="${SUDO}apt remove --auto-remove"
 alias apt-purge="apt-remove --purge"
-alias apt-autoremove="${SUDO}apt autoremove --purge -y"
+alias apt-autoremove="${SUDO}apt autoremove --purge"
 alias apt-all="apt-update && apt-upgrade"
 alias apt-clean="${SUDO}apt-get clean"
 alias apt-markauto="${SUDO}apt-mark auto"
