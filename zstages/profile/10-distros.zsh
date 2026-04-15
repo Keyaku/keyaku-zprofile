3f781ce5a027e6c18da77dd7af33b2e9fa923ec5
António Sarmento <Keyaku@users.noreply.github.com>
2026-04-15 15:07:20 +0100

    Improved Debian support
    Debian is really disappointing me. Forcing me to replicate code available in other distros, Jesus.

--- zstages/profile/10-distros.zsh
+++ zstages/profile/10-distros.zsh
@@ -5,7 +5,27 @@
 ##############################################################################
 
 if whatami Debian; then
-	# Debian discriminately resets PATH in this file, so any paths added prior
-	# are discarded.
-	emulate sh -c "source /etc/profile"
+	# Debian uses ridiculous tricks to set PATH, PS1 and BASH, and loading /etc/profile.d.
+	# We can't have that, so I'll just use the tools from this environment to set things up.
+	addpath /usr/local/sbin /usr/local/bin /usr/sbin
+	_zsh_source_dir /etc/profile.d profile.d '*.sh'
+
+	# Snippet taken from Arch's /etc/profile:
+	# Source global bash config, when interactive but not posix or sh mode
+	if test "$BASH" && \
+		test "$PS1" && \
+		test -z "$POSIXLY_CORRECT" && \
+		test "${0#-}" != sh && \
+		test -r /etc/bash.bashrc
+	then
+		. /etc/bash.bashrc
+	fi
+
+	# Replicate Arch's /etc/profile.d/flatpak-bindir.sh, missing in Debian
+	if [[ ! -f /etc/profile.d/flatpak-bindir.sh ]]; then
+		addpath "$XDG_DATA_HOME/flatpak/exports/bin" /var/lib/flatpak/exports/bin
+	fi
+
+	# Avoid Debian's compinit at /etc/zsh/zshrc
+	skip_global_compinit=1
 fi
