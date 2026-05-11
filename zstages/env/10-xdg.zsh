5fb34ab39441cf0b7e712d43ea8c79877947342d
António Sarmento <Keyaku@users.noreply.github.com>
2026-05-11 18:24:30 +0100

    Added ibus support for Wayland sessions

--- zstages/env/10-xdg.zsh
+++ zstages/env/10-xdg.zsh
@@ -9,7 +9,7 @@ export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
 
 # Load user-dirs if available
-if [[ -s $XDG_CONFIG_HOME/user-dirs.dirs ]]; then
+if [[ -s "$XDG_CONFIG_HOME"/user-dirs.dirs ]]; then
 	_zsh_source_file "$XDG_CONFIG_HOME/user-dirs.dirs"
 # user-dirs fallback (only if running a DE. Otherwise, not much interest in having these set)
 elif [[ -n "$XDG_CURRENT_DESKTOP" ]]; then
@@ -21,4 +21,13 @@ elif [[ -n "$XDG_CURRENT_DESKTOP" ]]; then
 	[[ -z $XDG_PUBLICSHARE_DIR && -d $HOME/Public ]] && export XDG_PUBLICSHARE_DIR=$HOME/Public
 	[[ -z $XDG_TEMPLATES_DIR && -d $HOME/Templates ]] && export XDG_TEMPLATES_DIR=$HOME/Templates
 	[[ -z $XDG_VIDEOS_DIR && -d $HOME/Videos ]] && export XDG_VIDEOS_DIR=$HOME/Videos
+fi
+
+
+### Window managers
+if [[ "$XDG_SESSION_TYPE" == wayland ]] && (( ${+commands[ibus]} )); then
+	export GTK_IM_MODULE=wayland
+	export XMODIFIERS=@im=ibus
+	export QT_IM_MODULES="wayland;ibus"
+	export QT_IM_MODULE=ibus
 fi
