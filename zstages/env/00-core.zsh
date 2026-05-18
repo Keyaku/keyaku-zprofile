baefdea29484ac5c399cc6c062f108250c06df39
António Sarmento <Keyaku@users.noreply.github.com>
2026-05-18 16:42:36 +0100

    Revert "Unsetting `LD_LIBRARY_PATH` and `LD_PRELOAD` if either are `null`"
    This reverts commit d5d0dd6eb82d56bbb01fca9a88b09a48371eca84.

--- zstages/env/00-core.zsh
+++ zstages/env/00-core.zsh
@@ -3,9 +3,3 @@
 #######################################
 
 export SHORT_HOST=${SHORT_HOST:-${(%):-%m}}
-
-# Some remote-shell hosts leak JSON nulls as the literal string "null" into
-# LD_* vars, which can trip dynamic linkers on every exec. Scrub before
-# anything downstream invokes an external binary.
-[[ $LD_LIBRARY_PATH == null ]] && unset LD_LIBRARY_PATH
-[[ $LD_PRELOAD == null ]] && unset LD_PRELOAD
